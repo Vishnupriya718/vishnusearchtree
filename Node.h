6ce5ebec275d33a79a4e83d12da7b6a91dedb51d
Vishnupriya Chunduri <vishnupriyachunduri@Vishnupriyas-MacBook-Air-7.local>
2026-04-02 15:08:40 -0700

    first commit

---
+++ Node.h
@@ -0,0 +1,12 @@
+#ifndef NODE_H
+#define NODE_H
+
+// Structure for one tree node
+struct Node {
+
+    int data;        // value stored
+    Node* left;      // left child pointer
+    Node* right;     // right child pointer
+};
+
+#endif
---
+++ bst.h
@@ -0,0 +1,20 @@
+#ifndef BST_H
+#define BST_H
+
+#include "Node.h"
+
+// Function declarations
+
+Node* createNode(int value);
+
+Node* insert(Node* root, int value);
+
+bool search(Node* root, int value);
+
+Node* removeNode(Node* root, int value);
+
+Node* findMin(Node* root);
+
+void printTree(Node* root, int space);
+
+#endif
